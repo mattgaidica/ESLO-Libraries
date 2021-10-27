518f3fb3d9aed32e1341e40ffae612b7882624ac
Matt Gaidica <matt@gaidi.ca>
2021-10-27 19:35:31 -0400

    Update ESLO.h

--- ESLO/ESLO.h
+++ ESLO/ESLO.h
@@ -24,12 +24,13 @@
 #include <ADS129X.h>
 #include <Definitions.h>
 
-#define VERSION_LENGTH 3
-#define V_DROPOUT 2200000 // 1.8V reg goes down to 2.2V, empirically tested
-#define EEG_SAMPLING_DIV 2 // effective Fs = (250 / this number)
-#define SWA_BUF_LEN 2 * (250 / EEG_SAMPLING_DIV)
-#define PACKET_SZ_EEG SIMPLEPROFILE_CHAR4_LEN / 4
-#define PACKET_SZ_XL SIMPLEPROFILE_CHAR5_LEN / 4
+#define VERSION_LENGTH 		3
+#define V_DROPOUT 			2200000 // 1.8V reg goes down to 2.2V, empirically tested
+#define EEG_SAMPLING_DIV 	2 // effective Fs = (250 / this number)
+#define SWA_BUF_HALF_LEN 	64 //250 / EEG_SAMPLING_DIV
+#define SWA_BUF_LEN 		2 * SWA_BUF_HALF_LEN
+#define PACKET_SZ_EEG 		SIMPLEPROFILE_CHAR4_LEN / 4
+#define PACKET_SZ_XL 		SIMPLEPROFILE_CHAR5_LEN / 4
 
 #define ESLO_FAIL 0x00
 #define ESLO_PASS 0x01
