85b50ebaa6256c35d4f16d17d0dca095e4d0fa64
Matt Gaidica <matt@gaidi.ca>
2022-02-23 10:29:27 -0500

    Update ESLO.h

--- ESLO/ESLO.h
+++ ESLO/ESLO.h
@@ -28,7 +28,8 @@
 #define V_DROPOUT 			2200000 // 1.8V reg goes down to 2.2V, empirically tested
 #define EEG_SAMPLING_DIV 	2 // effective Fs = (250 / this number)
 #define EEG_FS				250 // Hz (ideal)
-#define EEG_STARTUP_DELAY	500 // ms
+#define EEG_STARTUP_SAMPLES	(EEG_FS / EEG_SAMPLING_DIV) * 0.5 // seconds
+#define SPEAKER_CONN_DELAY	500 // ms
 #define FFT_HALF_LEN 		1024
 #define FFT_LEN 			2 * FFT_HALF_LEN
 #define FFT_SWA_DIV			2 // !! REMOVE TO FFT UPGRADE
